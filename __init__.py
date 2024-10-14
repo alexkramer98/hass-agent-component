5c01cc31f35a3e6f79c625548150d62599b2bbef
alex <alex@keicreations.nl>
2024-10-14 23:35:42 +0200

    remove unnecessary code

--- __init__.py
+++ __init__.py
@@ -1,47 +1,14 @@
-"""The OpenAI Conversation integration."""
+"""The Custom Hass Agent."""
 from __future__ import annotations
-
-import logging
 from typing import Literal
 
 from homeassistant.components import conversation
 from homeassistant.config_entries import ConfigEntry
-from homeassistant.const import CONF_API_KEY, MATCH_ALL
-from homeassistant.core import (
-    HomeAssistant,
-    ServiceCall,
-    ServiceResponse,
-    SupportsResponse,
-)
-from homeassistant.exceptions import (
-    ConfigEntryNotReady,
-    HomeAssistantError,
-    TemplateError,
-)
-from homeassistant.helpers import (
-    config_validation as cv,
-    intent,
-    issue_registry as ir,
-    selector,
-    template,
-)
-from homeassistant.helpers.typing import ConfigType
-from homeassistant.util import ulid
+from homeassistant.const import MATCH_ALL
+from homeassistant.core import HomeAssistant
+from homeassistant.helpers import intent
 
-from .const import (
-    CONF_CHAT_MODEL,
-    CONF_PROMPT,
-    CONF_TEMPERATURE,
-    DEFAULT_CHAT_MODEL,
-    DEFAULT_PROMPT,
-    DEFAULT_TEMPERATURE,
-    DOMAIN,
-)
-
-_LOGGER = logging.getLogger(__name__)
-SERVICE_GENERATE_IMAGE = "generate_image"
-
-CONFIG_SCHEMA = cv.config_entry_only_config_schema(DOMAIN)
+from .const import DOMAIN
 
 async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
     conversation.async_set_agent(hass, entry, HassAgent(hass, entry))
@@ -69,7 +36,7 @@ class HassAgent(conversation.AbstractConversationAgent):
         response = await self.hass.services.async_call(
             "rest_command",
             "conversate",
-            {"msg_id":user_input.conversation_id,"dev_id":user_input.device_id,"msg":user_input.text},
+            {"msg_id":user_input.conversation_id, "dev_id":user_input.device_id, "msg":user_input.text},
             blocking=True,
             return_response=True,
         )
--- const.py
+++ const.py
@@ -1,9 +1,2 @@
 DOMAIN = "hass_agent"
-CONF_PROMPT = "prompt"
-DEFAULT_PROMPT = """
-Standaard prompt
-"""
-CONF_CHAT_MODEL = "chat_model"
-DEFAULT_CHAT_MODEL = "mistral"
-CONF_TEMPERATURE = "temperature"
-DEFAULT_TEMPERATURE = 0.2
+NAME = "Hass Agent"
